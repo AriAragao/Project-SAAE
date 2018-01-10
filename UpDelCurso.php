38075e765c4608faacc5ff2015d6516c3c430c3d
Ariane Aragao <arisouza_aragao@outlook.com>
2018-01-10 10:56:09 -0200

    Fatal error: Uncaught Error: Cannot use object of type Curso as array in E:\Project-SAAE\UpDelCurso.php:9 Stack trace: #0 {main} thrown in E:\Project-SAAE\UpDelCurso.php on line 9

--- UpDelCurso.php
+++ UpDelCurso.php
@@ -1,107 +1,69 @@
 <?php
-
-   require_once './Class/DAO/CursoDao.php';
-   require_once './Class/DAO/CursoDao.php';
+       
+    require_once './Class/Entidade/Curso.php';
+    require_once './Class/DAO/CursoDao.php';
     
-   $curso = new CursoDao();
-   $cursoDao = new CursoDao();
+    $curso = new Curso();
+    $cursoDao = new CursoDao();
 
-   $idC= $curso["curso_codigo"];
+    $idC= $curso["curso_codigo"];
     var_dump($curso->Selecionar($idC));
-
-
-    /*$conn = new conexao;
-    $link = $conn->Conecta();
-
-    
-    $result_curso = "SELECT * FROM curso WHERE id = '4'";
-    $resultado_curso = mysqli_query($link, $result_curso);
-    $row_curso = mysqli_fetch_assoc($resultado_curso);  */
-
+       
 ?>
 
 <!DOCTYPE html>
-
 <html lang="pt-br">
 
-<head>
+    <head>
 
-	<title>Curso</title>
+        <title>Administrador</title>
+        <meta charset="utf-8">
+        <link rel="stylesheet" type="text/css" href="css/style.css"/>
 
-	<meta charset="utf-8">
+    </head>
 
+    <body>
 
-	<link rel="stylesheet" type="text/css" href="css/style.css"/>
+        <div id="Divtopo">
 
-	
-</head>
+            <img src="img/logoIf.png" id="logoIf">
+            <span id="lbIfba">Instituto Federal da Bahia </span>
 
-<body>
+        </div>
 
-            
-    <script>
-    
-    alert("Alteração realizada com sucesso!");
-    
-    </script>
-            
-        <?php }else if(isset($_POST['ExcluirC'])){ ?>
+        <div id="DivInicio">
+
+            <div id="DivTitulos">
+
+                <span id="titulo">SAEE</span>
+                <h1 id="tituloCompleto" >Sistema de Acompanhamento de Estágio e Emprego</h1>
+                <span id="tituloCoes">COORDEENAÇÃO DE ESTÁGIO</span>
+
+            </div>      
+
+        </div>
         
-            <script>
+        <div id="DivMenu">
 
-            alert("Cadastro deletado com sucesso!");
+            <ul id="listaMenu">
 
-            </script>
+                <li><a href="pgEmpresa.html">Empresa |</a></li>
+                <li><a href="PgCadProfessor.php" > Professor | </a> </li>
+                <li> <a href="PgCadCurso.php">Curso |</a></li>
+                <li> <a href="pgEstagiario.html">Estágio | </a></li>
                 
-        <?php }?>
-   
-	
-	
-    <div id="Divtopo">
+            </ul>
+        </div>
+        
+        <div id="DivCorpo">
 
-        <img src="img/logoIf.png" id="logoIf">
-        <span id="lbIfba">Instituto Federal da Bahia </span>
-
-    </div>
-	<div id="DivInicio">
-
-
-		
-		<div id="DivTitulos">
-
-            <span id="titulo">SAEE</span>
-            <h1 id="tituloCompleto" >Sistema de Acompanhamento de Estágio e Emprego</h1>
-            <span id="tituloCoes">COORDEENAÇÃO DE ESTÁGIO</span>
-
-         </div>    
-
-	</div>
-	<div id="DivMenu">
-		
-		<ul id="listaMenu">
-			
-                    <li><a href="pgEmpresa.php" class="ativa">Empresa |</a></li>
-			<li><a href="pgProfessor.php"> Professor | </a> </li>
-                        <li> <a href="pgCurso.php">Curso |</a></li>
-                        <li> <a href="pgEstagiario.php">Estágio | </a></li>
-		</ul>
-
-
-
-
-	</div>
-	<div id="DivCorpo">
- 
-	
-    <div id="divCabcalhoFormP">
-
-                <label id="lbDelProf">Dados do curso</label>
+            <div id="divCabcalhoFormP">
+                <label id="lbCadCurso">Alterar Curso</label>
             </div>
-        <div id="divFormP">
-
-            <div id="divOrgFormC">
-
-             <form action=" " method="POST">
+            
+            <div id="divFormP">
+                <div id="divOrgFormP">
+                    <form method="POST">
 
                 <label id="lbNomeC">Id</label>
                 <input type="text" name="inpIdC" id="inpIdC"  value="<?php echo $curso["curso_codigo"]; ?>"><br><br>
@@ -113,48 +75,45 @@
                 <label id="lbModC">Modalidade</label>
                 <input type="text" name="InpModalC" id="InpModalC" value="<?php echo $curso["curso_modalidade"]; ?>">
 
-                
-                <button id="EditarEmp" name="EditarC"> Atualizar</button>
-                <button id="btVoltarEmp"><a href="pgCurso.php" class="ativa">Voltar</a></button>
 
-              
-             </form>
 
-         </div>
+               <input type="submit" name="btCadastrar" value="Cadastrar" id="btCadastrarC" >                        
+               <button id="btVoltarC"><a href="pgCurso.php" class="ativa">Voltar</a></button>
 
+                    </form>
+                </div>
+                <div style="text-align: center">
+                    <br>
+                    <p id="resultadoCadastro"></p>
+                </div>
+            </div>
         </div>
-+
-	</div>
-
-
-</body>
-
+    </body>
 </html>
 
-<?php 
+<?php
 
-    if (isset($_POST["EditarC"])) {
-
-        $curso->setCurso_nome($_POST["inpNomeC"]);
-        $curso->setCurso_modalidade($_POST["InpModalC"]);
-        $curso->setCurso_codigo($_POST["inpIdC"]);
-
-        if ($cursoDao->Alterar($curso)) {
-            ?>    
-            <script type = "text/javascript">
-                document.getElementById("resultadoCadastro").innerHTML = "Cadastrado alterado com sucesso.";
-                document.getElementById("resultadoCadastro").style.color = "green";
-            </script>
-            <?php
-        } else {
-            ?>
-            <script type = "text/javascript">
-                document.getElementById("resultadoCadastro").innerHTML = "Erro ao alterar cadastro.";
-                document.getElementById("resultadoCadastro").style.color = "red";
-            </script>
-            <?php
-        }
-    }
-
-    }
+if (isset($_POST["btCadastrar"]))
+{
+    $curso->setCurso_nome($_POST["nome"]);
+    $curso->setCurso_modalidade($_POST["modalidade"]);
+   
+        
+    if ($cursoDao->Cadastrar($curso))
+    {
+?>    
+        <script type = "text/javascript">
+                    document.getElementById("resultadoCadastro").innerHTML = "Cadastrado com sucesso.";
+                    document.getElementById("resultadoCadastro").style.color = "green";
+        </script>
+<?php
+    } else 
+    {
 ?>
+        <script type = "text/javascript">
+                    document.getElementById("resultadoCadastro").innerHTML = "Erro ao cadastrar.";
+                    document.getElementById("resultadoCadastro").style.color = "red";
+        </script>
+<?php        
+    }
+}
